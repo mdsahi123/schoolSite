58f11dc169dc9e48564131d8e1e29c5cade9eb52
MD SAHI ALAM KHAN <88125879+mdsahi123@users.noreply.github.com>
2022-07-05 23:34:04 +0530

    first commit

---
+++ app/js/active.js
@@ -0,0 +1,12 @@
+// Slick nav active code 
+$(function(){
+    $('#nav').slicknav();
+});
+
+// Slick slider
+// $('.slider__container').slick({
+//     lazyLoad: 'ondemand',
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+//   });
+          
